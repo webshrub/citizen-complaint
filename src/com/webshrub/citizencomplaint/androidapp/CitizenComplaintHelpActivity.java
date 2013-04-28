da244e189297d214c19c9ccdb9811eb3a7668d86
Ahsan Javed <ahsan_cse2004@yahoo.com>
2013-04-28 15:26:09 +0530

    Initial checkin - Ahsan

---
+++ src/com/webshrub/citizencomplaint/androidapp/CitizenComplaintHelpActivity.java
@@ -0,0 +1,18 @@
+package com.webshrub.citizencomplaint.androidapp;
+
+import android.os.Bundle;
+import com.actionbarsherlock.app.SherlockActivity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ahsan.Javed
+ * Date: 4/28/13
+ * Time: 3:11 PM
+ */
+public class CitizenComplaintHelpActivity extends SherlockActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.citizen_complaint_help_activity);
+    }
+}
--- src/com/webshrub/citizencomplaint/androidapp/CitizenComplaintPreferenceActivity.java
+++ src/com/webshrub/citizencomplaint/androidapp/CitizenComplaintPreferenceActivity.java
@@ -1,13 +1,37 @@
 package com.webshrub.citizencomplaint.androidapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-import com.actionbarsherlock.app.SherlockPreferenceActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import com.actionbarsherlock.app.SherlockActivity;
 
-public class CitizenComplaintPreferenceActivity extends SherlockPreferenceActivity {
+public class CitizenComplaintPreferenceActivity extends SherlockActivity implements AdapterView.OnItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
+        setContentView(R.layout.citizen_complaint_preference_activity);
+        ListView listView = (ListView) findViewById(R.id.listView1);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
+        switch (position) {
+            case 0:
+                Intent helpIntent = new Intent(CitizenComplaintPreferenceActivity.this, CitizenComplaintHelpActivity.class);
+                startActivity(helpIntent);
+                break;
+            case 1:
+                Intent aboutIntent = new Intent(CitizenComplaintPreferenceActivity.this, CitizenComplaintAboutActivity.class);
+                startActivity(aboutIntent);
+                break;
+            case 2:
+                Intent profileIntent = new Intent(CitizenComplaintPreferenceActivity.this, CitizenComplaintProfileActivity.class);
+                startActivity(profileIntent);
+                break;
+        }
     }
 }
